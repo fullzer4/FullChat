afb8b8396da8cc03a5116e42ab2a40bb65ea8105
fullzer4 <gabrielpelizzaro@gmail.com>
2023-12-24 17:05:16 -0300

    new

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix flake for creating dev shells";
+  inputs = {
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { systems
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
+          f nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+          ];
+        };
+      });
+    };
+}
+
